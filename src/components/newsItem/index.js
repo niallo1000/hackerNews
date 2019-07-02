8db76ce7e2eba3ba4003ed66d5f81f68b7a318d2
LAPTOP-QP7T4FMV\niall <20082280@mail.wit.ie>
2019-07-02 21:59:14 +0100

    Comment Page integrated

--- src/components/newsItem/index.js
+++ src/components/newsItem/index.js
@@ -2,6 +2,10 @@ import React, { Component, Fragment } from 'react';
 import './newsItem.css';
 import "../../fontawesome";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from 'react-router-dom';
+
+
+
 
 export default class NewsItem extends Component {
     handleVote = () =>  this.props.upvoteHandler(this.props.post.id);
@@ -20,7 +24,7 @@ export default class NewsItem extends Component {
                 <span className="newsitem">
                     {line}
                     <span>
-                        <a href="/">Comments</a>
+                     <Link to={`/posts/${this.props.post.id}` }>Comments</Link>
                     </span>
                 </span>
                 <p className="author">{this.props.post.author}</p>
