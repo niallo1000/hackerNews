7014e984281b873ad96e007dc2645f7f1e0165f2
LAPTOP-QP7T4FMV\niall <20082280@mail.wit.ie>
2019-07-02 21:53:17 +0100

    Comment Page stories

--- stories/index.js
+++ stories/index.js
@@ -5,6 +5,9 @@ import NewsForm from '../src/components/newsForm/';
 import NewsItem from '../src/components/newsItem';
 import NewsList from '../src/components/newsList';
 import { action } from "@storybook/addon-actions";
+import CommentForm from "../src/components/commentForm";
+import Comment from "../src/components/comment";
+import CommentList from "../src/components/commentList";
 
 
 const post = {
@@ -39,3 +42,31 @@ storiesOf('Hacker App/News List', module)
 storiesOf("Hacker App/News Item", module)
     .add("default", () => <NewsItem post={post} upvoteHandler={action("upvoted")} /> )
     .add("No hyperlink", () => <NewsItem post={{ ...post, link: "" }} upvoteHandler={action("upvoted")}/>);
+
+
+storiesOf("Hacker App/Comment page/Comment Form", module).add("default", () => (
+    <CommentForm post={post} addCommentHandler={action("comment added")} />
+));
+
+const comment = {
+    id: 1,
+    author: "Joe Bloggs",
+    comment: "I agree with .....",
+    upvotes: 10
+};
+
+storiesOf("Hacker App/Comment page/Comment", module).add("default", () => (
+    <Comment upvoteHandler={action("upvoted")} comment={comment} />
+));
+
+storiesOf("Hacker App/Comment page/Comment list", module).add("default", () => {
+    const defaultComments = [
+        comment,
+        { ...comment, author: "Jane Smith", upvotes: 3, id: 2 },
+        { ...comment, comment: "On the other hand", id: 3 },
+        { ...comment, author: "Jill Dwyer", upvotes: 5, id: 4 }
+    ];
+    return (
+        <CommentList upvoteHandler={action("upvoted")} comments={defaultComments} />
+    );
+});
